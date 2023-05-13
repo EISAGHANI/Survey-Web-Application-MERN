f23b8d91fb6ac840802fcbbedc18bbd9d96e0e0a
EISAGHANI <eisabacha.com>
2023-05-13 14:02:16 +0200

    firt commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+const app = express();
+
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authroutes')(app);
+
+
+
+
+
+const PORT = process.env.PORT || 5500;
+app.listen(PORT);
